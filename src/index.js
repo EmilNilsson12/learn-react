7e7d791f4a91d764753e6b3f9e6f2a9e18ae9fc3
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-06-07 15:10:05 +0200

    Showcase JSX format

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', null, 'Hellow world!');
+const element = <h1>Hello world!!!!!!</h1>;
+// Babel compiles the code above INTO the code below:
+// const element = React.createElement('h1', null, 'Helldow world!');
 
 ReactDOM.render(element, document.getElementById('root'));
