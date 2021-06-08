e33193b77594fb9d715b53f163c491f9f45d403f
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-06-08 10:33:47 +0200

    Using React props for dynamic content

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import Greeting from '../greeting/greeting';
 
 class Main extends Component {
 	render() {
-		return <main>Main content!!!!!!!!!!!!!</main>;
+		return (
+			<>
+				<Greeting username={'Hellen'} />
+				<Greeting username={'Sofie'} />
+				<Greeting username={'Emma H'} />
+				<Greeting username={'Johanna'} />
+				<Greeting username={'Anna-Johanna'} />
+				<Greeting username={'Martina'} />
+				<Greeting username={'Ammi'} />
+				<Greeting username={'Lina'} />
+				<Greeting username={'Emma B'} />
+				<Greeting username={'Hanna'} />
+			</>
+		);
 	}
 }
 export default Main;
